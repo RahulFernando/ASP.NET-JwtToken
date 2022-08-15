ef81ecac62553d4e78896186074067975ed2762a
RahulFernando <rahulrulz680@gmail.com>
2022-08-15 12:53:23 +0530

    init

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using JwtToken.Models;
+using JwtToken.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtToken.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private IConfiguration _config;
+        private string SecurityAlorithms;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+
+                return Ok(token);
+            }
+
+            return NotFound("User not found!");
+        }
+
+        private string Generate(User user)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"], 
+                _config["Jwt:Audience"], 
+                claims, expires: DateTime.Now.AddMinutes(15), 
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User? Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(u => u.Username.ToLower() == userLogin.Username.ToLower() && u.Password == userLogin.Password);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Models/UserConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtToken.Models
+{
+    public class UserConstants
+    {
+        public static List<User> Users = new List<User>()
+        {
+            new User() { Id = 1, Username = "jason_admin", EmailAddress = "jasonadmin@gmai.com", Role = "Administrator", GivenName = "Jason", Surname = "Bryatt", Password = "admin_pass"},
+            new User() { Id = 2, Username = "elyse_seller", EmailAddress = "eslyseseller@gmai.com", Role = "Seller", GivenName = "Elyse", Surname = "Seller", Password = "elyse_pass"},
+        };
+    }
+}
